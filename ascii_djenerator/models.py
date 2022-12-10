2404164f08dd4f66fa27022180286c9fe5a4e2a7
Krushil Naik <krushilnaik96@gmail.com>
2022-12-09 20:16:05 -0500

    Create letter model

--- ascii_djenerator/models.py
+++ ascii_djenerator/models.py
@@ -1,3 +1,17 @@
+"""
+Models
+"""
+
 from django.db import models
 
 # Create your models here.
+
+
+class Letter(models.Model):
+    """
+    Representation of each character that makes up an "art".
+    "|"-separated for easy splitting interweaving
+    """
+
+    letter = models.CharField(max_length=1)
+    representation = models.CharField(max_length=49)
