a9b221bc70e331003047cf75e86d348b0f227d1b
Krushil Naik <krushilnaik96@gmail.com>
2022-12-13 15:02:11 -0500

    Reupload deleted files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    // templates
+    "./**/templates/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
